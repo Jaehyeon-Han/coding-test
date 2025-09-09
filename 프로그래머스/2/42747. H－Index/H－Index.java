8f837c41479f00fdcd3fa4e3dae2b8184a2c5234
Jaehyeon-Han <jaehyeonhan99@gmail.com>
2025-09-10 08:58:45 +0900

    [level 2] Title: H-Index, Time: 1.25 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,12 +4,7 @@ class Solution {
     public int solution(int[] citations) {
         Arrays.sort(citations);
 
-        if (citations[0] == citations[citations.length - 1]) {
-            return Math.min(citations[0], citations.length);
-        }
-
         int h = 0;
-
         for (int i = citations.length - 1; i >= 0; i--, h++) {
             int goeCount = citations.length - i;
 
