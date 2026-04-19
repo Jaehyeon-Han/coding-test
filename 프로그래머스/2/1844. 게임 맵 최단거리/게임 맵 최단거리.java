44f2d711134dff363dcc273661952b7f836dc1c0
Jaehyeon Han <jaehyeonhan99@gmail.com>
2026-04-19 19:29:32 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.13 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,55 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] maps) {
-        int distance = 0;
-        
-        int rows = maps.length;
-        int cols = maps[0].length;
-        
-        int[] dr = {-1,1,0,0};
-        int[] dc = {0,0,-1,1};
-        
-        int[][] dist = new int[rows][cols];
-        boolean[][] visited = new boolean[rows][cols];
-
-        Queue<int[]> q = new ArrayDeque<>();
-
-        q.add(new int[]{0, 0});
-        visited[0][0] = true;
-        dist[0][0] = 0;
-
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int r = cur[0];
-            int c = cur[1];
-
-            for (int d = 0; d < 4; d++) {
-                int nr = r + dr[d];
-                int nc = c + dc[d];
-
-                if (nr < 0 || nr >= rows || nc < 0 || nc >= cols) continue;
-                if (visited[nr][nc]) continue;
-                if (maps[nr][nc] == 0) continue;
-                
-                visited[nr][nc] = true;
-                dist[nr][nc] = dist[r][c] + 1;
-                q.add(new int[]{nr, nc});
-                
-                if (nr == rows - 1 && nc == cols - 1) {
-                    return dist[nr][nc] + 1;
-                }
-            }
-}
-        
-        return -1;
-    }
-    
-    static class Point {
-        int x, y;
-        Point(int x, int y) {
-            this.x=x;
-            this.y=y;
-        }
+        int answer = 0;
+        return answer;
     }
 }
